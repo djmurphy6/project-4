e1000439138abd64213abc7a31921632abbbb3ef
Dylan Murphy <dmurphy6@uoregon.edu>
2023-11-06 21:27:32 -0800

    added more test cases

--- brevets/tests/test_acp_times.py
+++ brevets/tests/test_acp_times.py
@@ -29,5 +29,35 @@ def test_open_200():
     expected_open_time = start_time.shift(hours=5, minutes=53)
     assert open_time(200, 200, start_time) == expected_open_time
 
+def test_open_50():
+    start_time = arrow.get("2023-11-06T00:00:00")
+    expected_open_time = start_time.shift(hours=1, minutes=28)
+    assert open_time(50, 200, start_time) == expected_open_time
+
+def test_open_150():
+    start_time = arrow.get("2023-11-06T00:00:00")
+    expected_open_time = start_time.shift(hours=4, minutes=23)
+    assert open_time(150, 300, start_time) == expected_open_time
+
+def test_open_400():
+    start_time = arrow.get("2023-11-06T00:00:00")
+    expected_open_time = start_time.shift(hours=11, minutes=45)
+    assert open_time(400, 400, start_time) == expected_open_time
+
+def test_close_50():
+    start_time = arrow.get("2023-11-06T00:00:00")
+    expected_close_time = start_time.shift(hours=3, minutes=20)
+    assert close_time(50, 200, start_time) == expected_close_time
+
+def test_close_150():
+    start_time = arrow.get("2023-11-06T00:00:00")
+    expected_close_time = start_time.shift(hours=8)
+    assert close_time(150, 300, start_time) == expected_close_time
+
+def test_close_400():
+    start_time = arrow.get("2023-11-06T00:00:00")
+    expected_close_time = start_time.shift(hours=22, minutes=40)
+    assert close_time(400, 400, start_time) == expected_close_time
+
 if __name__ == '__main__':
     nose.run()
